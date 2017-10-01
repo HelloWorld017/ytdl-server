b5fb5dc2a9059c35da57617f77f9e56bdd4cea23
Khinenw <deu07115@gmail.com>
2017-10-01 13:53:53 +0900

    Changed port to 80

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ytdl = require('ytdl-core');
 
 const app = new Koa();
 const config = require('./config.json');
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '80';
 
 const mergeDownload = async (v) => {
 	const url = `https://youtube.com/watch?v=${v}`;
